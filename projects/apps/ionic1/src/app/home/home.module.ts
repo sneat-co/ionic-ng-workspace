72beafcd1b93b2f7e72c79632a842cf83c44bbda
a_trakhimenok <a_trakhimenok@dellteam.com>
2019-12-04 22:12:57 +0000

    Examples of usage service from a package and shared component from code referenced through `tsconfog.json`

--- projects/apps/ionic1/src/app/home/home.module.ts
+++ projects/apps/ionic1/src/app/home/home.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {IonicModule} from '@ionic/angular';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
-import { HomePage } from './home.page';
+import {HomePage} from './home.page';
+import {SharedComponentsModule} from 'shared-code/components/shared-components.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
-  declarations: [HomePage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        SharedComponentsModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: HomePage
+            }
+        ])
+    ],
+    declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule {
+}
--- projects/apps/ionic1/src/app/home/home.page.ts
+++ projects/apps/ionic1/src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Lib0Service} from 'lib0';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    public foo0: string;
 
+    constructor(
+        lib0Service: Lib0Service,
+    ) {
+        this.foo0 = lib0Service.foo0();
+    }
 }
